ffd207e5e971a80e2f501c85d4076b6679b517c9
Caylee Stewart <caylee.stewart@gmail.com>
2017-07-19 17:39:38 -0400

    added buttons and changed fonts

--- js/index.js
+++ js/index.js
@@ -3,11 +3,14 @@ $(function() {
 	$(window).on("load resize", function () {
 		$(".fill-screen").css("height", window.innerHeight);
 	});
-//	js for animated menu button
+});
+
+$(function() {
+	//	js for animated menu button
 	 $(".navbar-toggle").on("click", function () {
 				    $(this).toggleClass("active");
 	 });
-	// sliding carousel
+		// sliding carousel
 	$('.carousel').carousel();
 	$('#myCarousel').on('slide.bs.carousel', function () {
   });
