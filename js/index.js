106db4cd1989de449ffa537db1cd9694b4097b7c
Caylee Stewart <caylee.stewart@gmail.com>
2017-08-18 00:23:48 -0400

    cleaned up css

--- js/index.js
+++ js/index.js
@@ -1,25 +1,31 @@
-// allows hero to fill the screen
+
+
 $(function () {
+	// allows hero to fill the screen
 	$(window).on("load resize", function () {
 		$(".fill-screen").css("height", window.innerHeight);
 	});
 	//initialize WOW for animation
 	new WOW().init();
-});
-//	js for animated menu button
-$(function () {
 	$(".navbar-toggle").on("click", function () {
-	$(this).toggleClass("active");
+	  $(this).toggleClass("active");
 	});
 		// sliding carousel
 	$('.carousel').carousel();
 	$('#myCarousel').on('slide.bs.carousel', function () {
   });
+	//hover over quote
+	$(".hover-over").hover(function () {
+		$(this).find(".hover-over-container").show();
+	}, function () {
+		$(this).find(".hover-over-container").hide();
+	});
+	// smooth scrolling
+	$("nav a").bind("click", function () {
+		$("html, body").stop().animate({
+			scrollTop: $($(this).attr("href")).offset().top
+		}, 1500, "easeInOutExpo");
+		event.preventDefault();
+	});
 });
-//hover over quote
-	$(".hover-over").hover(function() {
-			$(this).find(".hover-over-container").show();
-		}, function() {
-			$(this).find(".hover-over-container").hide();
-		});
 
